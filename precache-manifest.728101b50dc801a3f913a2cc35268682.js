d71421dcab4797b26ce61e53aad5562dc98b0309
Fandanguito <43961556+Fandanguito@users.noreply.github.com>
2020-05-16 12:34:50 -0400

    Updates

---
+++ precache-manifest.728101b50dc801a3f913a2cc35268682.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1582a3a616480e4e9502c43bc0d41d4e",
+    "url": "/Playlist-downloader/index.html"
+  },
+  {
+    "revision": "04e323347e737d4d1cd6",
+    "url": "/Playlist-downloader/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "85a753778bcb8779e80d",
+    "url": "/Playlist-downloader/static/js/2.c8653838.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Playlist-downloader/static/js/2.c8653838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04e323347e737d4d1cd6",
+    "url": "/Playlist-downloader/static/js/main.df8dbd8f.chunk.js"
+  },
+  {
+    "revision": "d95f16d8295d0f46cfae",
+    "url": "/Playlist-downloader/static/js/runtime-main.178f6299.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Playlist-downloader/static/media/logo.5d5d9eef.svg"
+  }
+]);
